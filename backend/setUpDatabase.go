5c6a1f30d9968902f7bda5589d68dfc2bbfe3aa0
apatil <apatil@ihmc.org>
2023-07-25 21:51:04 -0500

    Adding proper go mod initialization

---
+++ backend/setUpDatabase.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"ConcurrentBookingSystem/models"
+
+	"gorm.io/driver/mysql" // Or any other database driver you are using
+	"gorm.io/gorm"
+)
+
+func initializeDatabase() {
+	db, err := gorm.Open(mysql.Open("your-database-connection-string"), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to the database")
+	}
+
+	err = db.AutoMigrate(&models.City{})
+	if err != nil {
+		panic("Failed to auto-migrate the table")
+	}
+
+}
