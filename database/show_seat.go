68042f7f8dbc55377d3c559f05ab845332165aac
anmollp <anmollp@outlook.com>
2023-08-17 23:14:31 -0500

    Fixed ticket queries.

--- database/show_seat.go
+++ database/show_seat.go
@@ -1,18 +1,17 @@
 package database
 
+import "github.com/GatorsTigers/ConcurrentBookingSystem/models"
+
 func GetTotalBookingAmount(showSeatIds []uint32) float64 {
 	var amount float64
-	var query = "select sum(price) as amount from show_seats where show_seat_id in ?"
-	if txn := DbInstance.Db.Raw(query, showSeatIds).Scan(&amount); txn.Error != nil {
-		panic(txn.Error)
+	if err := DbInstance.Db.Model(&models.ShowSeat{}).Select("sum(price)").Where("show_seat_id in ?", showSeatIds).Row().Scan(&amount); err != nil {
+		panic(err)
 	}
 	return amount
 }
 
 func UpdateShowSeats(ticketId uint32, showSeatIds []uint32) bool {
-	var query string = "update show_seats set ticket_refer_id = ? and is_available = false where show_seat_id in ?"
-	txn := DbInstance.Db.Raw(query, ticketId, showSeatIds)
-	if txn.Error != nil {
+	if txn := DbInstance.Db.Model(&models.ShowSeat{}).Where("show_seat_id in ?", showSeatIds).UpdateColumns(models.ShowSeat{TicketReferId: ticketId, IsAvailable: false}); txn.Error != nil {
 		panic(txn.Error)
 	}
 	return true
